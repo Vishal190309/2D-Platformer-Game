9c32666ccef816cbee6791af2a188b0cd104a53b
Vishal190309 <52538851+Vishal190309@users.noreply.github.com>
2024-06-23 01:53:24 +0530

    Assignment 1 - Crouch

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -6,6 +6,9 @@ public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
     public Animator animator;
+    private bool crouched = false;
+    private Vector2 normalColliderSize = new Vector2(0.5507991f, 2.073027f);
+    private Vector2 crouchedColliderSize = new Vector2(0.7272125f, 1.358973f);
     void Start()
     {
         
@@ -15,10 +18,12 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         float speed = Input.GetAxisRaw("Horizontal");
+        crouch(Input.GetKeyDown(KeyCode.LeftControl));
         Debug.Log(speed);
         if (animator != null)
         {
             animator.SetFloat("Speed",Mathf.Abs(speed));
+            animator.SetBool("Crouch", crouched);
         }
         Vector3 scale = transform.localScale;
         if (speed < 0f)
@@ -31,5 +36,26 @@ public class PlayerController : MonoBehaviour
         }
         transform.localScale = scale;
 
+
+
+    }
+
+    void crouch(bool crouch)
+    {
+        if (crouch)
+        {
+            crouched = !crouched;
+            if (crouched)
+            {
+                GetComponent<BoxCollider2D>().size = crouchedColliderSize;
+                GetComponent<BoxCollider2D>().offset = new Vector2(GetComponent<BoxCollider2D>().offset.x, GetComponent<BoxCollider2D>().offset.y -0.4f);
+            }
+            else
+            {
+                GetComponent<BoxCollider2D>().size = normalColliderSize;
+                GetComponent<BoxCollider2D>().offset = new Vector2(GetComponent<BoxCollider2D>().offset.x, GetComponent<BoxCollider2D>().offset.y + 0.4f);
+
+            }
+        }
     }
 }
