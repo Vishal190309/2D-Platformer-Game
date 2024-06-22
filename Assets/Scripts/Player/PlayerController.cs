f7e1ce77119ec2af954c161c8200e1494602a9cb
Vishal190309 <52538851+Vishal190309@users.noreply.github.com>
2024-06-23 01:19:10 +0530

    Scripting Animation & Flipping

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
+    public Animator animator;
     void Start()
     {
         
@@ -13,6 +14,22 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float speed = Input.GetAxisRaw("Horizontal");
+        Debug.Log(speed);
+        if (animator != null)
+        {
+            animator.SetFloat("Speed",Mathf.Abs(speed));
+        }
+        Vector3 scale = transform.localScale;
+        if (speed < 0f)
+        {
+           scale.x = -1f * Mathf.Abs(speed);
+        }
+        else if(speed>0f)
+        {
+            scale.x = Mathf.Abs(speed);
+        }
+        transform.localScale = scale;
+
     }
 }
