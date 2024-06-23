88dafa2c12b5d0c127d087212e50e9605f1861e0
Vishal190309 <52538851+Vishal190309@users.noreply.github.com>
2024-06-23 17:59:31 +0530

    Assignment - Implementation of Animated Keys

--- Assets/Scripts/Level/KeyController.cs
+++ Assets/Scripts/Level/KeyController.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class KeyController : MonoBehaviour
 {
+    private Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = gameObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -16,12 +17,25 @@ public class KeyController : MonoBehaviour
         
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+
+    private IEnumerator OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.GetComponent<PlayerController>() != null)
         {
+            gameObject.GetComponent<BoxCollider2D>().enabled = false;
+            animator.Play("Base Layer.KeyFadeOut",0,0.25f);
             PlayerController playerController = collision.gameObject.GetComponent<PlayerController>();
             playerController.PickUpKey();
+
+            float counter = 0;
+            float waitTime = 0.4f;
+            //Now, Wait until the current state is done playing
+            while (counter < (waitTime))
+            {
+                
+                counter += Time.deltaTime;
+                yield return null;
+            }
             Destroy(gameObject);
         }
 
